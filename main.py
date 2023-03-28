76b3cc03a9cdf5665b744171945f7ae6dafb0a10
Pavich123 <pavlyuchenko0209@gmail.com>
2023-03-28 13:17:16 +0200

    updated getData function and added writeFile function

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+
+
+#function for checking identity
 def check_our_files(path1, path2):
     str1 = getData(path1)
     str2 = getData(path2)
-    print(str1,str2)
 
+    
+#get data from the wile
 def getData(path):
     file = open(path,'r')
     result = []
 
     for str in file:
+        str = str.replace("\n", "")
         result.append(str)
     file.close()
     return result
-
-
+#write data in the file
+def writeFile(path:str, data):
+    result_file = open(path, "w")
+    result_file.truncate()
+    for el in data:
+        result_file.write(el+"\n")
+    result_file.close()
+    
+#start of the function
 check_our_files("file1.txt", "file2.txt")
