f3a802a3165b21a883d7b241e234fc551b9e7083
Pavich123 <pavlyuchenko0209@gmail.com>
2023-03-28 13:18:27 +0200

    added checkForSame and checkForDiff functions

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+#check for the same
+def checkForSame(str1, str2):
+    same = []
+    for el in str1:
+        if el in str2:
+            same.append(el)
+    return same
 
+#check for the difference
+def checkForDiff(str1, str2):
+    diff = []
+    for el in str1:
+        if el not in str2:
+            diff.append(el)
+    return diff
 
 #function for checking identity
 def check_our_files(path1, path2):
